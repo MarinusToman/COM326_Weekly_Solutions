3cb87fc24cd461ea0f7647ea9dc15c6e14b71f2c
Marinus Toman <103184442+MarinusToman@users.noreply.github.com>
2023-09-29 09:44:07 +0100

    Week 1 - Challenge 1

---
+++ Week1/Q1/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Q1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, what is your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("What is your age?");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"Hello {name}, you will be {(age + 5)} in 5 years");
+        }
+    }
+}
