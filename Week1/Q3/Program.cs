cea8257d387209f8e6fc1819908614dbb6f9aab6
Marinus Toman <103184442+MarinusToman@users.noreply.github.com>
2023-09-29 09:44:38 +0100

    Challenge 3

---
+++ Week1/Q3/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Q3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Option 1: using implicit casting in the For loop statement
+            for (int i = 'A'; i <= 'z'; i++)
+            {
+                // If the value is between 91-96, we don't want to print
+                if(!(i > 90 && i <= 96))
+                {
+                    Console.WriteLine($"{i}\t{(char) i}"); // we use explicit casting here
+                }
+                    
+            }
+
+            // Option 2: 
+            for(int i = 65;  i <= 90;  i++)
+            {
+                Console.WriteLine($"{i}\t{(char) i}\t{(i+32)}\t{(char) (i+32)}"); // print all on one line
+            }
+        }
+    }
+}
