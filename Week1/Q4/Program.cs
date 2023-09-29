ecaffd17caf2182c99b8fab6e713b93704dad8f0
Marinus Toman <103184442+MarinusToman@users.noreply.github.com>
2023-09-29 09:44:45 +0100

    Challenge 4

---
+++ Week1/Q4/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Q4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Get user input
+            Console.WriteLine("Please enter a number greater than 1:");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            // Loop through each value i to n
+            for(int i = 1; i <= n; i++)
+            {
+                // Check user input
+                if ((i % 3 == 0) && (i %5 != 0)) 
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if ((i % 3 != 0) && ((i % 5 == 0)))
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else if ((i % 3 == 0) && (i % 5 == 0))
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else 
+                { 
+                    Console.WriteLine(i); 
+                }
+            }
+        }
+    }
+}
